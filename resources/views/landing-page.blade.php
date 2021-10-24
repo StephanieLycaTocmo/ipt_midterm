af72316a890e8d4852b6bbaa7909fdb74a4145c1
unknown <lycatocmo57@gmail.com>
2021-10-24 21:07:37 +0800

    first commit

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,48 @@
+
+@extends('base')
+
+@section('content')
+
+<style>
+  img {
+  width: 100%;
+  height: auto;
+}
+
+.jumbotron {
+    background: url("Beach.jpg") no-repeat center;
+}
+.btn-primary {
+    background-color: #c60063;
+    border-color: #c60063;
+}
+
+.btn-secondary {
+    background-color: transparent;
+    border-color: #c60063;
+    color: #c60063;
+}
+
+#services {
+    background: url(".jpg");
+}
+
+body{
+   background-image:url('alicia.jpg');
+   background-size: cover;
+}
+
+
+</style>
+
+<br>
+<body>
+   <section id="services" class="container">
+      <h2 class="display-4 text-center mt-5 mb-3">Welcome to CheapTalk!</h2>
+      <h5 style="font-weight: 400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse fuga, iure accusantium nihil quasi labore enim excepturi officia sed doloremque reprehenderit facilis autem quae, rem voluptate dolores? Obcaecati, maxime iure.</h5>
+      
+    </section>
+    
+</body>
+
+
